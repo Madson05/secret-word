bb4d9d7a12f866cfc3aba10e92b85d202388f263
madson <madsonsillva2005@gmail.com>
2022-11-30 10:16:41 -0300

    adicionado a estrutura jsx no componente do jogo

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,9 +2,33 @@ import "./Game.css"
 
 const Game = ({verifyLetter}) => {
   return (
-    <div>
-      <h1>Game</h1>
-      <button onClick={verifyLetter}>Proxima fase</button>
+    <div className="game">
+      <p className="points">
+        <span>Pontuação: 00</span>
+      </p>
+      <h1>Advinhe a palavra:</h1>
+      <h3 className="tip">
+        Dica sobre a palavra: <span>dica...</span>
+      </h3>
+
+      <div className="wordContainer">
+        <span className="letter">A</span>
+        <span className="blankSquare"></span>
+      </div>
+
+
+      <div className="letterContainer">
+        <p>Tente advinhar uma letra da palavra: </p>
+        <form>
+          <input type="text" name="letter" maxLength={1} required/>
+          <button>Jogar!</button>
+        </form>
+      </div>
+      <div className="wrongLettersContainer">
+        <p>Letras já utilizadas</p>
+        <span>a,</span>
+        <span>b,</span>
+      </div>
     </div>
   )
 }
