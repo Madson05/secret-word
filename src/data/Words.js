d89ceb7364ca618d17ee5183fc7365eec3555bcd
madson <madsonsillva2005@gmail.com>
2022-12-01 11:05:00 -0300

    add palavras e alterado idioma padrão

--- src/data/Words.js
+++ src/data/Words.js
@@ -1,8 +1,8 @@
 export const wordsList = {
-    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena"],
-    fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja"],
-    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
-    computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
-    programação: ["Linguagem", "Framework", "JavaScript", "React"],
-    alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
+    carro: ["Motor", "Porta", "Capô", "Pneu", "Antena", "Motor"],
+    fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja", "Melancia"],
+    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos", "Nariz", "Cabeça"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Processador"],
+    programação: ["Linguagem", "Framework", "JavaScript", "React", "Lógica"],
+    alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo", "Frango", "Porco", "Soja", "Biscoito"],
   };
